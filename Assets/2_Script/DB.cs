3c57035e7eb2b2df72fafe8182a779e6252db511
purelykiss <ov13@naver.com>
2024-10-14 15:43:18 +0900

    DB 연결 시도

--- Assets/2_Script/DB.cs
+++ Assets/2_Script/DB.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Data;
+//using Oracle.DataAccess.Client;
+//using UnityEngine;
 
-public class DB : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//public class DB : MonoBehaviour
+//{
+//    private string connectionString = "User Id=hr;Password=hr;Data Source=localhost:152/xe;";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//    void Start()
+//    {
+//        ConnectToDatabase();
+//    }
+
+//    void ConnectToDatabase()
+//    {
+//        using (OracleConnection connection = new OracleConnection(connectionString))
+//        {
+//            try
+//            {
+//                connection.Open();
+//                Debug.Log("Database connection successful!");
+
+//                // 데이터 쿼리 예시
+//                string query = "SELECT * FROM yourTable";
+//                using (OracleCommand command = new OracleCommand(query, connection))
+//                {
+//                    using (OracleDataReader reader = command.ExecuteReader())
+//                    {
+//                        while (reader.Read())
+//                        {
+//                            Debug.Log(reader["columnName"].ToString());
+//                        }
+//                    }
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Debug.LogError("Error connecting to database: " + ex.Message);
+//            }
+//        }
+//    }
+//}
